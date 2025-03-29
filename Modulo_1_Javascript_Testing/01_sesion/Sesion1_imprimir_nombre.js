2c1fac36784731584fa3b36a56200283a7ba4a7a
jvaladezo <32528527+jvaladezo@users.noreply.github.com>
2025-03-28 19:16:30 -0600

    Sesion 1
    Imprimir nombre

---
+++ Modulo_1_Javascript_Testing/01_sesion/Sesion1_imprimir_nombre.js
@@ -0,0 +1,13 @@
+var nombre = "José Luis";
+var apellido = 'Valadez';
+var nombreCompleto = `${nombre} ${apellido}`
+var edad = 36;
+
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} años`);
+console.log("Hola, soy " + nombreCompleto +" y tengo " + edad + " años");
+
+//Comparacion 
+// ==, ===, !=, <, >, >=, <=;
+// var numero = 5;
+// var numerotexto = 5;
+// console.log(numero === numerotexto);
