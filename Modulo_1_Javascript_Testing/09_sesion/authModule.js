b8f8a2f667e2b2a9846a97c89882008f5d958466
jvaladezo <32528527+jvaladezo@users.noreply.github.com>
2025-04-07 00:14:07 -0600

    Clase 9
    Login Ficticio

---
+++ Modulo_1_Javascript_Testing/09_sesion/authModule.js
@@ -0,0 +1,28 @@
+const AuthModule = (function () {
+    //Simular Usuario
+    const userDB = {
+        username: "admin",
+        password: "1234"
+    }
+    let currentUser = null;
+
+    return {
+        login(username, password) {
+            if (username === userDB.username && password === userDB.password) {
+                currentUser = UserSingleton.getInstance(username);
+                console.log(`Usuario Autentificado: ${currentUser.name}`)
+            } else {
+                console.log("Credenciales incorrectas")
+            }
+        },
+        logout() {
+            if (currentUser) {
+                console.log(`Hasta luego, ${currentUser.name}`)
+                currentUser = null;
+                UserSingleton.clearInstance();
+            } else {
+                console.log("No hay usuario autentificado");
+            }
+        }
+    }
+})();
