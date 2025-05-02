4f3bc0e8edf6b144acb3cc2d46c9cadba1db02b5
jvaladezo <32528527+jvaladezo@users.noreply.github.com>
2025-05-01 23:53:17 -0600

    Clase 3
    Templete y Databulding

---
+++ Modulo_2_Angular/03_sesion/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Saludo } from './componets/saludo/saludo.components';
+import { cardComponent } from './components/card/card.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Saludo, cardComponent, TarjetaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name = 'José Luis Valadez Olivo';
+  dato = 'Desarrollador se Software';
+  dato2= 140;
+}
