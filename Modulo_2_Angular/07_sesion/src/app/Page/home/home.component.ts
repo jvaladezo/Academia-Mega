85fc62838718e9865a077af7af662183588f3e23
jvaladezo <32528527+jvaladezo@users.noreply.github.com>
2025-05-07 01:54:15 -0600

    Clase 7
    Componente hijo

---
+++ Modulo_2_Angular/07_sesion/src/app/Page/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { HijoComponent } from '../../components/hijo/hijo.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [HijoComponent, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  
+usuarios=[
+  {nombre:"Juan", edad:25,profesion:"Ingeniero"},
+  {nombre:"Ana", edad:25,profesion:"Diseñadora"},
+  {nombre:"Luis", edad:25,profesion:"Estudiante"},
+]
+
+}
---
+++ Modulo_2_Angular/07_sesion/src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { cardComponent } from './components/card/card.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
+import { ProductsManagerComponent } from './components/products-manager/products-manager.component';
+import { HomeComponent } from './Page/home/home.component';
+import { ErrorComponent } from './Page/error/error.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'gestor',
+        component: ProductsManagerComponent
+    },
+    {
+        path:'card',
+        component: cardComponent
+    },
+    {
+        path:'todo',
+        component: TodoComponent
+    },
+    {
+        path:'tarjeta',
+        component: TarjetaComponent
+    },
+    {
+        path:'**',
+        component: ErrorComponent
+    },    
+];
