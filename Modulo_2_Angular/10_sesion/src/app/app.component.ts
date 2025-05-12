790bc0c9f15c5292588d0644ff51f2738bd764e3
jvaladezo <32528527+jvaladezo@users.noreply.github.com>
2025-05-11 19:00:31 -0600

    clase 10
    onInit y docheck
    ademas de case hijo y padre

---
+++ Modulo_2_Angular/10_sesion/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Saludo } from './componets/saludo/saludo.components';
+import { cardComponent } from './components/card/card.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
+import { CommonModule } from '@angular/common';
+import { TodoComponent } from './components/todo/todo.component';
+import { ProductsManagerComponent } from './components/products-manager/products-manager.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Saludo, cardComponent, TarjetaComponent, CommonModule, TodoComponent,ProductsManagerComponent, RouterLink ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  rolUsuario = 'alguien';
+  edad = 3
+
+  tareaImportante = true;
+
+  isUrgente = false;
+
+
+}
