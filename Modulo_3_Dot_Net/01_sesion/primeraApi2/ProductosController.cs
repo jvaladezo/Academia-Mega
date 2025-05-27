dd254c0e460cad77e0c13d94e31460c69e2b87c2
jvaladezo <32528527+jvaladezo@users.noreply.github.com>
2025-05-26 23:42:19 -0600

    Primer entregable SaludoController

---
+++ Modulo_3_Dot_Net/01_sesion/primeraApi2/ProductosController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]//Api/
+
+public class ProductosController : ControllerBase
+{
+    public static readonly List<Producto> _datos = new()
+    {
+        new Producto { id = 1, Nombre = "iPhone 16", Precio = 20000.0m},
+        new Producto { id = 2, Nombre = "Galaxy s25 Edge", Precio = 20000.0m}
+    };
+
+    
+    /*
+        - CREATE -
+    */
+    [HttpPost] // POST /api/productos
+    public ActionResult<Producto> Create(Producto nuevo)
+    {
+        nuevo.id = _datos.Max(p => p.id) +1;
+        _datos.Add(nuevo);
+        return CreatedAtAction(nameof(GetById), new{id = nuevo.id},nuevo);
+    }
+
+    /*
+        - READ -
+    */
+
+    [HttpGet]
+
+    public ActionResult<IEnumerable<Producto>> GetAll()
+    {
+        return Ok(_datos);
+    }
+
+    [HttpGet("{id}")]//GET /api/productos/1
+
+    public ActionResult <Producto> GetById(int id)
+    {
+        var producto = _datos.FirstOrDefault(p => p.id == id);
+        if (producto == null) return NotFound();
+        return Ok(producto);
+    }
+    [HttpPut("{id}")]// GET PUT /api/productos/1
+    public IActionResult Update(int id, Producto actualizado)
+    {
+         var product = _datos.FirstOrDefault(p => p.id == id);
+        if (product == null) return NotFound();
+
+        product.Nombre = actualizado.Nombre;
+        product.Precio = actualizado.Precio;
+
+        return NoContent();
+    }
+
+}
+public class Producto {
+    public int id {get; set;}
+
+    public string Nombre {get; set;} = string.Empty;
+
+    public decimal Precio {get; set;}
+}
